09c1749f746f733875eb6893c90ced9a6afdb937
mohamadrzaa86-max <mohamadrzaa86@gmail.com>
2026-07-14 03:21:41 +0330

    Add files via upload

---
+++ AdminState.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramMovieBot.Models
+{
+    public class AdminState
+    {
+        public string? FileId { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+
+        public string? PhotoFileId { get; set; }
+        public bool WaitingForDeleteCode { get; set; }
+        public bool WaitingForEditCode { get; set; }
+        public Movie? EditingMovie { get; set; }
+        public string? Mode { get; set; }
+        public bool WaitingForBroadcast { get; set; }
+      
+
+        public bool WaitingForMovie { get; set; }
+        public bool WaitingForTitle { get; set; }
+        public bool WaitingForDescription { get; set; }
+    }
+}
