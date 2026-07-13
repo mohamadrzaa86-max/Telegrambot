3b42fc37c6c1f8825a90a22d1c87da46f52d144f
mohamadrzaa86-max <mohamadrzaa86@gmail.com>
2026-07-14 03:14:52 +0330

    Add files via upload

---
+++ Movie.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramMovieBot.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        public string MovieCode { get; set; } = string.Empty;
+
+        public string Title { get; set; } = string.Empty;
+
+        public string FileId { get; set; } = string.Empty;
+
+        public string? Description { get; set; }
+        public string MediaType { get; set; } = string.Empty;
+        public string? PhotoFileId { get; set; }
+        public int Views { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramMovieBot.Models
+{
+    public class User
+    {
+        public long TelegramId { get; set; }
+
+        public string? Username { get; set; }
+
+        public string? FirstName { get; set; }
+        public int Id { get; internal set; }
+    }
+}
