38f9e349cdc4a378b34738b93e2871a7aad31c8f
mohamadrzaa86-max <mohamadrzaa86@gmail.com>
2026-07-16 04:20:49 +0330

    Update AdminState.cs

--- AdminState.cs
+++ AdminState.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,5 +23,6 @@ namespace TelegramMovieBot.Models
         public bool WaitingForMovie { get; set; }
         public bool WaitingForTitle { get; set; }
         public bool WaitingForDescription { get; set; }
+        public bool WaitingForChannelUsername { get; set; }
     }
 }
